39cc8aca13d2db9ee646ba6416412b100ae71fbb
Triane Peart <TrianePeart@gmail.com>
2023-06-06 23:35:44 -0400

    updated forumControl

--- controllers/ForumController.js
+++ controllers/ForumController.js
@@ -8,6 +8,16 @@ const {
   updateForum,
 } = require("../queries/forums");
 
+const RepliesController = require('./RepliesController')
+// const categoryController = require('./categoryController')
+// const TopicController = require('./TopicController')
+
+
+forum.use('/:forumId/replies', RepliesController)
+// forums.use('/:forumId/categories', categoryController)
+// forums.use('/:forumId/topics', TopicController)
+
+
 // Get all forums
 forum.get("/", async (req, res) => {
   try {
@@ -45,26 +55,6 @@ forum.post("/", async (req, res) => {
     res.status(500).json({ error: "Error creating forum" });
   }
 });
-
-// Create a new forum
-// forum.post("/", async (req, res) => {
-//   try {
-//     const { forum_title, forum_description, forum_created_at, forum_posts, user_id, category_id } = req.body;
-//     const newForum = await createForum({ 
-//       title: forum_title,
-//       description: forum_description,
-//       created_at: forum_created_at,
-//       posts: forum_posts,
-//       user_id,
-//       category_id
-//     });
-//     res.status(201).json(newForum);
-//   } catch (error) {
-//     console.error("Error creating forum:", error);
-//     res.status(500).json({ error: "Internal Server Error" });
-//   }
-// });
-
 
 // Delete a forum
 forum.delete("/:id", async (req, res) => {
